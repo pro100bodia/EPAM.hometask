c55be820eb405778f8222b136aae760dc90ec36d
Bodia <bohdanpukhalsky@ukr.net>
2019-03-21 15:57:38 +0200

    First task add

---
+++ part1/Task1.java
@@ -0,0 +1,9 @@
+public class Task1{
+	public static void main(String[] args){
+		try{
+        	System.out.println(Integer.parseInt(args[0], 2));
+		}catch(NumberFormatException e){
+			e.printStackTrace();
+		}
+	}
+}
